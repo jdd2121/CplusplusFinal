87a1a0099bbc0c15fda7dd76f6604e9cda0c0cb0
Landon Schumacher <landonschumacher6@gmail.com>
2026-05-01 17:57:01 -0500

    actually added product class hopefully

---
+++ product.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include "product.h"
+
+product::product(std::string name, double price, int stock, int ID) {
+	setName(name);
+	setPrice(price);
+	setStock(stock);
+	setID(ID);
+}
+product::product() {
+	this->name = "N/A";
+	this->price = 0.00;
+	this->stock = 0;
+	this->ID = 0;
+}
+
+void product::setName(std::string newName) {
+	if (newName.length() <= 0) {
+		throw std::exception("Invalid Name - name cannot be empity");
+	}
+	else {
+		this->name = newName;
+	}
+}
+void product::setPrice(double newPrice) {
+	if (newPrice <= 0) {
+		throw std::exception("Invalid Price price must be more than 0");
+	}
+	else {
+		this->price = newPrice;
+	}
+}
+void product::setStock(int newStock) {
+	if (newStock < 0) {
+		throw std::exception("Invalid Stock - stock cannot be less than 0");
+	}
+	else {
+		this->stock = newStock;
+	}
+}
+void product::setID(int newID) {
+	this->ID = newID;
+}
+
+void product::increaseStock(int x) {
+	setStock(this->stock + x);
+}
+void product::decreaseStock(int x) {
+	setStock(this->stock - x);
+}
+
+const std::string product::getName() {
+	return this->name;
+}
+const double product::getPrice() {
+	return this->price;
+}
+const int product::getStock() {
+	return this->stock;
+}
+
+const void product::listProduct() {
+	std::cout << getName() << "\t\t$" << getPrice() << "\tStock:" << getStock() << std::endl;
+}
