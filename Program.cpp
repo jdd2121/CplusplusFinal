c5c1bc155fdd9b5d9465f4c83314a5b3d35d6046
Landon Schumacher <landonschumacher6@gmail.com>
2026-05-01 18:01:40 -0500

    test code in main .cpp

--- Program.cpp
+++ Program.cpp
@@ -1,7 +1,30 @@
+//Our program file for this project
 #include <iostream>
+#include <string>
+#include<iomanip>
+#include"product.h"
 
-int main() {
-	std::cout << "Test" << std::endl;
+void random() {
 
-	return 0;
+}
+
+
+int main()
+{
+    ////Print out a message to the user
+    //std::cout << "Hello, World!" << std::endl;
+
+    //std::cout << "Testing (JD)" << std::endl;
+    //std::cout << "Test #2" << std::endl;
+    //std::cout << "Test #3" << std::endl;
+
+    product products[3];
+    for (int i = 0;i < 3;i++) {
+        products[i].product::product();
+    }
+    for (int i = 0;i < 3;i++) {
+        products[i].listProduct();
+    }
+
+    return 0;
 }
