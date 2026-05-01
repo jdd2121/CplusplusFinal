ffd1b8a0a4a334751782c82c8c26b044330521cc
Landon Schumacher <landonschumacher6@gmail.com>
2026-05-01 17:59:14 -0500

    added product.h

---
+++ product.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class product {
+	std::string name;
+	double price;
+	int stock;
+	int ID; //The ID is what position it is in the array
+
+public:
+
+	product(std::string name, double price, int stock, int ID);
+	product();
+
+	void setName(std::string name);
+
+	void setPrice(double newPrice);
+
+	void setID(int newID);
+	void setStock(int newStock);
+	void increaseStock(int x);
+	void decreaseStock(int x);
+
+	void const listProduct();
+	std::string const getName();
+	double const getPrice();
+	int const getStock();
+};
